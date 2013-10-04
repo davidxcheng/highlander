4171158472a57995ffe611fa832b101fe9ebed66
David Cheng <davidxcheng@gmail.com>
2013-10-04 02:26:02 +0200

    added styling + began scraping of streaks

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
 		'davidxcheng',
 		'karlbohlmark',
 		'jonniehedkvist'
-	]
+	],
+	url: 'https://github.com/'
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,28 @@
 exports.index = function(req, res) {
-	var devs = require('../config').devs;
+
+	var config 	= require('../config'),
+		request = require('request'),
+		cheerio	= require('cheerio'),
+		q		= require('q');
+
+	var getStreak = function(username) {
+		request(config.url + username, function(err, resp, html) {
+			var $ = cheerio.load(html);
+			console.log($('.contrib-streak-current span.num').text());
+			return $('.contrib-streak-current span.num').text();
+		});
+	};
+
+	var devs = config.devs.map(function(name) {
+		var streak = getStreak(name);
+		return {
+			name: name,
+			streak: streak
+		};
+	});
+
+	console.log(devs);
+
 	res.render('index', { 
 		title: 'Highlander',
 		devs: devs
