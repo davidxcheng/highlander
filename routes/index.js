be43f28f84cde5d1672b4c1494c6b49dab13d5cf
David Cheng <davidxcheng@gmail.com>
2013-10-05 22:20:23 +0200

    changed font to ghost n goblins!

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,16 @@ exports.index = function(req, res) {
 		var devs = x.map(function(y) {
 			var $ 		= cheerio.load(y[0].body);
 			var name	= $('*[itemprop=additionalName]').text(),
-				streak 	= $('.contrib-streak-current span.num').text();
+				streak 	= $('.contrib-streak-current span.num').text().split(' ')[0];
 			
 			return {
 				name: name,
 				streak: streak
 			};
+		});
+
+		devs.sort(function(x, y) {
+			return x.streak < y.streak;
 		});
 
 		res.render('index', { 
