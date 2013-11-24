6805118e8f08ff80160fcf39f574e5ec7368345a
David Cheng <davidxcheng@gmail.com>
2013-11-25 00:54:42 +0100

    refactored and added comments for future ref

--- routes/index.js
+++ routes/index.js
@@ -5,39 +5,42 @@ exports.index = function(req, res) {
 		cheerio	= require('cheerio'),
 		Q		= require('q');
 
+	// create a version of request that returns promises
 	var qRequest = Q.denodeify(request);
 
-	function map(arr, iterator) {
-		var promises = arr.map(function(el) { 
-			return iterator(config.url + el);
+	function requestGithubProfiles(devs) {
+		var promises = devs.map(function(dev) {
+			return qRequest(config.url + dev);
 		});
 
 		// return a promise
 		return Q.all(promises);
 	};
 
+	function scrapeAndMap(results) {
+		var devs = results.map(function(y) {
+			var $ 		= cheerio.load(y[0].body),
+				name	= $('*[itemprop=additionalName]').text(),
+				streak 	= $('.contrib-streak-current span.num').text().split(' ')[0];
+
+			return {
+				name: name,
+				streak: streak
+			};
+		});
+
+		devs.sort(function(x, y) {
+			return x.streak < y.streak;
+		});
+
+		return devs;
+	}
+
 	// api for promise.then: .then(onFulfilled, onRejected, onProgress)
-	map(config.devs, qRequest)
-		.then(function(results) { 
-			var devs = results.map(function(y) {
-				var $ 		= cheerio.load(y[0].body);
-				var name	= $('*[itemprop=additionalName]').text(),
-					streak 	= $('.contrib-streak-current span.num').text().split(' ')[0];
-				
-				return {
-					name: name,
-					streak: streak
-				};
-			});
-
-			devs.sort(function(x, y) {
-				return x.streak < y.streak;
-			});
-
-			return devs;
-		})
+	requestGithubProfiles(config.devs)
+		.then(scrapeAndMap)
 		.done(function(devs) {
-			res.render('index', { 
+			res.render('index', {
 				title: 'Highlander',
 				devs: devs
 			});
